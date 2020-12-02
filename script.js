b0e690040ca091824bf44d410dac0634136af6a2
dvramos <dvramos@hotmail.com.br>
2020-12-02 00:45:57 -0300

    DIO 5/8

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const yourShip = document.querySelector('.player-shooter');
-const playArea = document.querySelector('.main-player-game');
+const playArea = document.querySelector('#main-play-area');
 
 //movimento e tiro da nave
 function flyAhip(event){
@@ -41,23 +41,31 @@ function moveDown(){
 // funcionalidade de tiro
 function fireLaser(){
     let laser = createLaserElement();
-    playArea.appendChild();
-    moveLaser();
+    playArea.appendChild(laser);
+    moveLaser(laser);
 }
 
 function createLaserElement(){
     let xPosition = parseInt(window.getComputedStyle(yourShip).getPropertyValue('left'));
     let yPosition = parseInt(window.getComputedStyle(yourShip).getPropertyValue('top'));
     let newLaser = document.createElement('img');
-    newLaser.scr = 'img/shoot.png';
+    newLaser.src = 'img/shoot.png';
     newLaser.classList.add('laser');
     newLaser.style.left = `${xPosition}px`
     newLaser.style.top = `${yPosition - 10}px`
     return newLaser;
 }
 
-function moveLaser(){
-    
+function moveLaser(laser){
+    let laserInterval = setInterval(() => {
+        let xPosition = parseInt(laser.style.left);
+
+        if(xPosition === 340) {
+            laser.remove();
+        } else {
+            laser.style.left = `${xPosition + 8}px`;
+        }
+    }, 10);
 }
 
 window.addEventListener('keydown', flyAhip);
